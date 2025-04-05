7173ad40951a02922e731f15ae58e52241caf7bb
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-04-04 23:17:05 -0500

    Initial Commit

---
+++ Assets/Scripts/Input/NewUIInput.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewUIInput : UIInput
+{
+    private PlayerInputActions playerInputActions;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        InitializePlayerInputActions();
+    }
+
+    private void InitializePlayerInputActions()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.UI.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputActions.UI.Disable();
+    }
+
+    public override bool CanProcessInput()
+    {
+        return true;
+    }
+
+    public override bool GetPauseDown()
+    {
+        if (!CanProcessInput()) return false;
+        if (InputOnCooldown()) return false;
+
+        bool pauseInput = playerInputActions.UI.Pause.WasPerformedThisFrame();
+        return pauseInput;
+    }
+}
