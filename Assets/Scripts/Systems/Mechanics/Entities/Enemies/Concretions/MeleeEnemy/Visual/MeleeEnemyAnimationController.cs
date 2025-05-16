a5e95847bd9c5492382df99d55ecb760536c2bf3
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-05-15 19:27:10 -0500

    ADD TestMeleeEnemy Anim1

---
+++ Assets/Scripts/Systems/Mechanics/Entities/Enemies/Concretions/MeleeEnemy/Visual/MeleeEnemyAnimationController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyAnimationController : EnemyAnimationController
+{
+    [Header("Melee Enemy Components")]
+    [SerializeField] private MeleeEnemyAttack meleeEnemyAttack;
+
+
+}
