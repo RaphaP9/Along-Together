b3edf89a4dff9a8f234dbb0bc323d462cd2b84e7
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-05-19 09:54:37 -0500

    ADD Sol1

--- Assets/Scripts/Systems/Mechanics/Entities/General/Visual/Weapons/EntityWeaponPivotHandler.cs
+++ Assets/Scripts/Systems/Mechanics/Entities/General/Visual/Weapons/EntityWeaponPivotHandler.cs
@@ -6,22 +6,27 @@ using UnityEngine;
 public class EntityWeaponPivotHandler : MonoBehaviour
 {
     [Header("Components")]
-    [SerializeField] private EntityAimDirectionerHandler aimDirectionerHandler;
+    [SerializeField] private MouseDirectionHandler mouseDirectionHandler;
     [SerializeField] private EntityHealth entityHealth;
+    [Space]
+    [SerializeField] private Transform weaponPivot;
     [SerializeField] private Transform refferenceAimPoint;
 
     [Header("Runtime Filled")]
-    [SerializeField] private Vector2 pivotAimRefferenceOffset;
     [SerializeField] private float pivotAimRefferenceAngle;
+    [SerializeField] private float pivotAimRefferenceDistance;
     [Space]
-    [SerializeField] private float aimAngle;
+    [SerializeField] private float pivotTargetAngle;
+    [SerializeField] private float pivotTargetDistance;
+    [Space]
     [SerializeField] private float pivotAngle;
 
     public float PivotAngle => pivotAngle;
 
     private void Start()
     {
-        CalculatePivotAimRefferenceOffset();
+        pivotAimRefferenceAngle = CalculatePivotAimRefferenceAngle();
+        pivotAimRefferenceDistance = CalculatePivotAimRefferenceDistance();
     }
 
     private void Update()
@@ -29,21 +34,42 @@ public class EntityWeaponPivotHandler : MonoBehaviour
         HandlePivotRotation();
     }
 
-    private void CalculatePivotAimRefferenceOffset()
-    {
-        pivotAimRefferenceOffset = refferenceAimPoint.localPosition;
-        pivotAimRefferenceAngle = GeneralUtilities.GetVector2AngleDegrees(pivotAimRefferenceOffset);
-    }
+    private float CalculatePivotAimRefferenceAngle() => GeneralUtilities.GetVector2AngleDegrees(GeneralUtilities.SupressZComponent(refferenceAimPoint.position - weaponPivot.position));
+    private float CalculatePivotAimRefferenceDistance() => GeneralUtilities.SupressZComponent(refferenceAimPoint.position - weaponPivot.position).magnitude;
+    private float CalculatePivotTargetAngle() => GeneralUtilities.GetVector2AngleDegrees(GetTargetPosition() - GeneralUtilities.SupressZComponent(weaponPivot.position));
+    private float CalculatePivotTargetDistance() => (GetTargetPosition() - GeneralUtilities.SupressZComponent(weaponPivot.position)).magnitude;
+
+
+    private Vector2 GetTargetPosition() => mouseDirectionHandler.Input;
+
 
     private void HandlePivotRotation()
     {
         if (!entityHealth.IsAlive()) return;
 
-        UpdateRotation();
+        //Update Values
+
+        pivotTargetAngle = CalculatePivotTargetAngle();
+        pivotTargetDistance = CalculatePivotTargetDistance();
+
+        //Components of the equation
+
+        float phi = pivotAimRefferenceAngle;
+        float AB = pivotAimRefferenceDistance;
+
+        float alpha = pivotTargetAngle;
+        float AC = pivotTargetDistance;
+
+        float beta = alpha - Mathf.Asin((AB/AC)*Mathf.Sin(phi*Mathf.Deg2Rad));
+
+        pivotAngle = beta;
+
+        UpdatePivotRotation();
     }
 
-    private void UpdateRotation()
+    private void UpdatePivotRotation()
     {
-        aimAngle = aimDirectionerHandler.AimAngle;
+        weaponPivot.rotation = Quaternion.Euler(0f, 0f, pivotAngle);
     }
+
 }
