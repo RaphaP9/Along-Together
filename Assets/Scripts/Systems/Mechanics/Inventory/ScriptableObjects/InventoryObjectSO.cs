a0087ae5ffaa935c04c63dd036316d306d411b06
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-05-29 13:34:44 -0500

    ADD GoldCounter On Shop

--- Assets/Scripts/Systems/Mechanics/Inventory/ScriptableObjects/InventoryObjectSO.cs
+++ Assets/Scripts/Systems/Mechanics/Inventory/ScriptableObjects/InventoryObjectSO.cs
@@ -10,8 +10,10 @@ public abstract class InventoryObjectSO : ScriptableObject, IHasEmbeddedNumericS
     public Rarity objectRarity;
     [TextArea(3,10)] public string description;
     public Sprite sprite;
-    [Space]
+
+    [Header("Prices")]
     [Range(0, 1000)] public int price;
+    [Range(0, 1000)] public int sellPrice;
 
     [Header("Numeric Embedded Stats")]
     public List<NumericEmbeddedStat> numericEmbeddedStats;
