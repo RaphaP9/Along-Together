d2d1b935df7d5091b9dde2a9bb86e797ae607fcd
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-06-18 13:01:33 -0500

    ADD Legato Starting&Ending

--- Assets/Scripts/Systems/Mechanics/Abilities/Concretions/Delice/Legato/Legato.cs
+++ Assets/Scripts/Systems/Mechanics/Abilities/Concretions/Delice/Legato/Legato.cs
@@ -10,10 +10,14 @@ public class Legato : ActiveAbility, IDodger, IAttackInterruption
 
     private LegatoSO LegatoSO => AbilitySO as LegatoSO;
 
+    public event EventHandler OnLegatoStarting;
     public event EventHandler OnLegatoStart;
+    public event EventHandler OnLegatoEnding;
     public event EventHandler OnLegatoCompleted;
 
+    public static event EventHandler OnAnyLegatoStarting;
     public static event EventHandler OnAnyLegatoStart;
+    public static event EventHandler OnAnyLegatoEnding;
     public static event EventHandler OnAnyLegatoCompleted;
 
     public float LegatoTimer { get; private set; }
@@ -31,6 +35,14 @@ public class Legato : ActiveAbility, IDodger, IAttackInterruption
     protected override void HandleFixedUpdateLogic() { }
     protected override void HandleUpdateLogic() { }
     #endregion
+
+    public override bool CanCastAbility()
+    {
+        if (!base.CanCastAbility()) return false;
+        if (isCurrentlyActive) return false;
+
+        return true;
+    }
 
     protected override void OnAbilityCastMethod()
     {
@@ -52,19 +64,32 @@ public class Legato : ActiveAbility, IDodger, IAttackInterruption
     private IEnumerator LegatoCoroutine()
     {
         isCurrentlyActive = true;
+        ResetTimer();
+
+        OnAnyLegatoStarting?.Invoke(this, EventArgs.Empty);
+        OnLegatoStarting?.Invoke(this, EventArgs.Empty);
+
+        Debug.Log("LegatoStarting");
+
+        yield return new WaitForSeconds(LegatoSO.flyStartDuration);
 
         OnAnyLegatoStart?.Invoke(this, EventArgs.Empty);
         OnLegatoStart?.Invoke(this, EventArgs.Empty);
 
         Debug.Log("LegatoStart");
 
-        ResetTimer();
-
         while (LegatoTimer < GetDuration())
         {
             LegatoTimer += Time.deltaTime;
             yield return null;
         }
+
+        OnAnyLegatoEnding?.Invoke(this, EventArgs.Empty);
+        OnLegatoEnding?.Invoke(this, EventArgs.Empty);
+
+        Debug.Log("LegatoEnding");
+
+        yield return new WaitForSeconds(LegatoSO.flyEndDuration);
 
         isCurrentlyActive = false;
 
@@ -73,10 +98,10 @@ public class Legato : ActiveAbility, IDodger, IAttackInterruption
         OnAnyLegatoCompleted?.Invoke(this, EventArgs.Empty);
         OnLegatoCompleted?.Invoke(this, EventArgs.Empty);
 
-        Debug.Log("LegatoEnd");
+        Debug.Log("LegatoCompleted");
     }
 
-    private float GetDuration()
+    private float GetDuration() //Maybe duration is not exactly FlyDuration (Longer duration due to Ability Level,etc)
     {
         return LegatoSO.flyDuration;
     }
--- Assets/Scripts/Systems/Mechanics/Abilities/Concretions/Delice/Legato/LegatoSO.cs
+++ Assets/Scripts/Systems/Mechanics/Abilities/Concretions/Delice/Legato/LegatoSO.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class LegatoSO : ActiveAbilitySO
 {
     [Header("Specific Settings")]
+    [Range(0f, 1f)] public float flyStartDuration;
     [Range(1f, 5f)] public float flyDuration;
+    [Range(0f, 1f)] public float flyEndDuration;
+    [Space]
     public PhysicPushData pushData;
 }
