2084f4973f5f41e2dc33a1324429149b38150b02
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-06-18 19:19:30 -0500

    ADD EnemyShadows

--- Assets/Scripts/Systems/Mechanics/Entities/Enemies/Visual/EnemyShadowHandler.cs
+++ Assets/Scripts/Systems/Mechanics/Entities/Enemies/Visual/EnemyShadowHandler.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class EnemyShadowHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Components")]
+    [SerializeField] private GameObject shadowGameObject;
+    [Space]
+    [SerializeField] private EnemySpawnHandler enemySpawnHandler;
+    [SerializeField] private EnemyHealth enemyHealth;
+
+    private void OnEnable()
     {
-        
+        enemySpawnHandler.OnEnemySpawnStart += EnemySpawningHandler_OnThisEnemySpawnStart;
+        enemySpawnHandler.OnEnemySpawnComplete += EnemySpawningHandler_OnThisEnemySpawnComplete;
+
+        enemyHealth.OnEnemyDeath += EnemyHealth_OnThisEnemyDeath;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        enemySpawnHandler.OnEnemySpawnStart -= EnemySpawningHandler_OnThisEnemySpawnStart;
+        enemySpawnHandler.OnEnemySpawnComplete -= EnemySpawningHandler_OnThisEnemySpawnComplete;
+
+        enemyHealth.OnEnemyDeath -= EnemyHealth_OnThisEnemyDeath;
+    }
+
+    private void EnableShadow() => shadowGameObject.SetActive(true);
+    private void DisableShadow() => shadowGameObject.SetActive(false);
+
+    private void EnemySpawningHandler_OnThisEnemySpawnStart(object sender, EnemySpawnHandler.OnEnemySpawnEventArgs e)
+    {
+        DisableShadow();
+    }
+
+    private void EnemySpawningHandler_OnThisEnemySpawnComplete(object sender, EnemySpawnHandler.OnEnemySpawnEventArgs e)
+    {
+        if (!enemyHealth.IsAlive()) return;
+        EnableShadow();
+    }
+
+    private void EnemyHealth_OnThisEnemyDeath(object sender, EnemyHealth.OnEntityDeathEventArgs e)
+    {
+        DisableShadow();
     }
 }
