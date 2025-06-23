5b7da90948830e45a2723d876eb9db1712f01b85
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-06-23 11:25:08 -0500

    ADD Faster Enemies

--- Assets/Scripts/Systems/Mechanics/Entities/Enemies/ScriptableObjects/BaseClasses/KamikazeEnemySO.cs
+++ Assets/Scripts/Systems/Mechanics/Entities/Enemies/ScriptableObjects/BaseClasses/KamikazeEnemySO.cs
@@ -7,7 +7,7 @@ public class KamikazeEnemySO : EnemySO
 {
     [Header("Kamikaze Enemy Settings")]
     [Range(0.5f, 10f)] public int explosionDamage;
-    [Range(0.5f, 3f)] public float explosionRadius;
+    [Range(0.5f, 5f)] public float explosionRadius;
     [Space]
     [Range(0.5f, 5f)] public float detectionRange;
     [Range(0.5f, 5f)] public float explosionTime;
