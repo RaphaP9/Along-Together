2ff38204018cb2e1d792b9d3f0eb3d7c797efe77
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-06-28 16:15:07 -0500

    ADD BonusDamageLowHealthEffect

---
+++ Assets/Scripts/Systems/Mechanics/TreatEffects/Concretions/BonusDamageLowHealthTreat/BonusDamageLowHealthTreatEffectHandler.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusDamageLowHealthTreatEffectHandler : TreatEffectHandler
+{
+    public static BonusDamageLowHealthTreatEffectHandler Instance { get; private set; }
+
+    private BonusDamageLowHealthTreatEffectSO BonusDamageLowHealthTreatEffectSO => treatEffectSO as BonusDamageLowHealthTreatEffectSO;
+
+    private PlayerHealth playerHealth;
+
+    private void OnEnable()
+    {
+        PlayerInstantiationHandler.OnPlayerInstantiation += PlayerInstantiationHandler_OnPlayerInstantiation;
+    }
+    private void OnDisable()
+    {
+        PlayerInstantiationHandler.OnPlayerInstantiation -= PlayerInstantiationHandler_OnPlayerInstantiation;
+    }
+
+    protected override void SetSingleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #region VirtualMethods
+    protected override void OnTreatEnablementByConditionMethod()
+    {
+        base.OnTreatEnablementByConditionMethod();
+        TemporalNumericStatModifierManager.Instance.AddStatModifiers(BonusDamageLowHealthTreatEffectSO.refferencialGUID, BonusDamageLowHealthTreatEffectSO);
+    }
+
+    protected override void OnTreatDisablementByConditionMethod() 
+    {
+        base.OnTreatDisablementByConditionMethod();
+        TemporalNumericStatModifierManager.Instance.RemoveStatModifiersByGUID(BonusDamageLowHealthTreatEffectSO.refferencialGUID);
+    }
+
+    protected override bool EnablementCondition() => playerHealth.CurrentHealth <= BonusDamageLowHealthTreatEffectSO.healthThreshold;
+    #endregion
+
+
+    private void PlayerInstantiationHandler_OnPlayerInstantiation(object sender, PlayerInstantiationHandler.OnPlayerInstantiationEventArgs e)
+    {
+        playerHealth = e.playerTransform.GetComponentInChildren<PlayerHealth>();
+    }
+}
