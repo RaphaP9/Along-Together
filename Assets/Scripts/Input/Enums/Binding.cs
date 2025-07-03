41768aee9f9cc9cce327988df18868c0afe6cd19
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-07-03 12:47:25 -0500

    ADD Bindings 1

---
+++ Assets/Scripts/Input/Enums/Binding.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Binding 
+{
+    Move_Up,
+    Move_Down, 
+    Move_Left, 
+    Move_Right,
+    
+    Attack,
+
+    Ability1, 
+    Ability2, 
+    Ability3,
+
+    SkipDialogue,
+
+    Stats,
+    DevMenu,
+    Pause
+}
--- Assets/Scripts/Input/NewInput/CentralizedInputSystemManager.cs
+++ Assets/Scripts/Input/NewInput/CentralizedInputSystemManager.cs
@@ -39,4 +39,61 @@ public class CentralizedInputSystemManager : MonoBehaviour
         PlayerInputActions = new PlayerInputActions();
         OnPlayerInputActionsInitialized?.Invoke(this , new OnPlayerInputActionsEventArgs { playerInputActions = PlayerInputActions });
     }
+
+    public void DisableAllActionMaps()
+    {
+        PlayerInputActions.UI.Disable();
+        PlayerInputActions.Movement.Disable();
+        PlayerInputActions.Attack.Disable();
+        PlayerInputActions.Abilities.Disable();
+        PlayerInputActions.Conversations.Disable();
+    }
+
+    public void EnableAllActionMaps()
+    {
+        PlayerInputActions.UI.Enable();
+        PlayerInputActions.Movement.Enable();
+        PlayerInputActions.Attack.Enable();
+        PlayerInputActions.Abilities.Enable();
+        PlayerInputActions.Conversations.Enable();
+    }
+
+    public string GetBindingText(Binding binding)
+    {
+        switch (binding)
+        {
+            default:
+            case Binding.Move_Up:
+                return PlayerInputActions.Movement.Move.bindings[1].ToDisplayString();
+            case Binding.Move_Down:
+                return PlayerInputActions.Movement.Move.bindings[2].ToDisplayString();
+            case Binding.Move_Left:
+                return PlayerInputActions.Movement.Move.bindings[3].ToDisplayString();
+            case Binding.Move_Right:
+                return PlayerInputActions.Movement.Move.bindings[4].ToDisplayString();
+            case Binding.Attack:
+                return PlayerInputActions.Attack.Attack.bindings[0].ToDisplayString();
+            case Binding.Ability1:
+                return PlayerInputActions.Abilities.AbilityA.bindings[0].ToDisplayString();
+            case Binding.Ability2:
+                return PlayerInputActions.Abilities.AbilityB.bindings[0].ToDisplayString();
+            case Binding.Ability3:
+                return PlayerInputActions.Abilities.AbilityC.bindings[0].ToDisplayString();
+            case Binding.SkipDialogue:
+                return PlayerInputActions.Conversations.Skip.bindings[0].ToDisplayString();
+            case Binding.Stats:
+                return PlayerInputActions.UI.Stats.bindings[0].ToDisplayString();
+            case Binding.DevMenu:
+                return PlayerInputActions.UI.DevMenu.bindings[0].ToDisplayString();
+            case Binding.Pause:
+                return PlayerInputActions.UI.Pause.bindings[0].ToDisplayString();
+        }
+    }
+
+    public void RebindBinding(Binding binding)
+    {
+        DisableAllActionMaps();
+
+        
+    }
 }
